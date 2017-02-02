2c93c9989362124996085655d8d6de5ee1fbafbb
Shadman Ahmed <shadman.ahmed001@gmail.com>
2017-02-02 15:43:43 -0800

    this is the final update  Eli did review with me

--- apps/belt_django_app/models.py
+++ apps/belt_django_app/models.py
@@ -1,120 +1,90 @@
 from __future__ import unicode_literals
 from django.db import models
-from django.contrib import messages
-from django.http import HttpResponse
 import bcrypt
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
+
 class UserManager(models.Manager):
-	def login(self, request):
-		email = request.POST['email']
-		password = request.POST['password']
-		x = False;
+	def login(self, post):
+		email = post['email'].lower().strip()
+		password = post['password']
+
+		errors =[]
 		if len(email) == 0:
-			messages.error(request, "email is required")
-			x = True;
+			errors.append('email is required')
 		elif not User.objects.filter(email = email).exists():
-			messages.error(request, "email is not in the database")
-			x=True;
-		if x:
-			return x
-		else:
-			x = False
-			password = password.encode()
+			errors.append('email is not in the database')
+
+		if not errors:
 			user_list = User.objects.filter(email = email)
 			user = user_list[0]
-			ps_hashed = user.password
-			ps_hashed = ps_hashed.encode()
+			password = password.encode()
+			ps_hashed = user.password.encode()
 			if bcrypt.hashpw(password, ps_hashed) == ps_hashed:
-				request.session['user_id'] = user.id
-				request.session['name'] = user.name
-				return x
+				return {'status': True, 'user_id': user.id, 'user_name' : user.name}
 			else:
-				messages.error(request, "email or password does not match")
-				x = True
-				return x
-		x = True
-		return x
-	def register(self, request):
-		if request.method == 'POST':
-			x = False
-			if not EMAIL_REGEX.match(request.POST['email']):
-				messages.info(request, ' Invalid email ')
-				x = True
-			if not NAME_REGEX.match(request.POST['name']):
-				messages.info(request, ' Invalid name ')
-				x = True
-			if not NAME_REGEX.match(request.POST['last']):
-				messages.info(request, ' Invalid last ')
-				x = True
-			if len(request.POST['password']) < 8:
-				messages.info(request,'Password must be atleast 8 characters long')
-				x = True
-			elif request.POST['password'] != request.POST['confirm_password']:
-				messages.info(request,'Password and confirm password are not matched')
-				x = True
-			if x:
-				return x
+				errors.append('email or password does not match')
+		return {'status': False, 'errors': errors}
 
-			else:
-				password = request.POST['password'].encode()
+	def register(self, post):
+		email = post['email'].lower().strip()
+		name = post['name']
+		last = post['last']
+		password = post['password']
+		confirm_password = post['confirm_password']
+
+		errors = []
+		if not EMAIL_REGEX.match(email):
+			errors.append(' Invalid email ')
+		if not NAME_REGEX.match(name):
+			errors.append(' Invalid name ')
+		if not NAME_REGEX.match(last):
+			errors.append(' Invalid last ')
+		if len(password) < 8:
+			errors.append(' Password must be atleast 8 characters long ')
+		elif password != confirm_password:
+			errors.append(' Password and confirm password are not matched ')
+
+		if not errors:
+			already_user_list = User.objects.filter(email=email)
+			if not already_user_list:
+				password = password.encode()
 				hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-				already_user_list= User.objects.filter(email=request.POST['email'])
-				if not already_user_list:
-					print "this means new user"
-					user = User.objects.create(name=request.POST['name'],last=request.POST['last'],email = request.POST['email'],password=hashed,)
-					print ('**************')
-					x = False
-					try:
-						request.session['name'] = request.POST['name']
-						request.session['user_id'] = user.id
-					except:
-						request.session['name'] = ""
-						request.session['user_id'] = 0
-					return x
+				print "this means new user"
+				user = User.objects.create(name=name,last=last,email = email,password=hashed,)
+				print ('**************')
+				return {'status': True, 'user_id': user.id}
+			else:
+				errors.append('Please login below. Your email already exists in our DB')
+				print 'this means its a returning user but logging in the wrong place'
 
-				else:
-					print 'this means its a returning user but logging in the wrong place'
-					messages.info(request, 'Please login below. Your email already exists in our DB')
-		x = True
-		return x
+		return {'status': False, 'errors': errors}
 
-	def addusertofriend(self, dataObject):
-		user_id = dataObject['user_id']
-		friend_id = dataObject['friend_id']
-		newfriendship_list = User.objects.filter(id=dataObject['user_id'])
-
-		newfriendshipperson_list = User.objects.filter(id=dataObject['friend_id'])
-
-		newfriendperson=newfriendshipperson_list[0]
-		newfriend=newfriendship_list[0]
-
-		newfriend.friends.add(newfriendperson)
-
-		print 'if it got till here it add the friend'
+	def addusertofriend(self, user_id, friend_id):
+		user_list = User.objects.filter(id = user_id)
+		friend_list = User.objects.filter(id = friend_id)
+		if friend_list:
+			friend = friend_list[0]
+			me = user_list[0]
+			me.friends.add(friend)
 		return True
 
-	def deletefriend(self, dataObject):
-
-		user_list=User.objects.filter(id=dataObject['friend_user_id'])
-		user2_list=User.objects.filter(id=dataObject['user_id'])
-
-		user = user_list[0]
-		user2 = user2_list[0]
-
-		test = user.friends.all()
-		user.friends.remove(user2)
-
-		print "got to delete function"
+	def deletefriend(self, myId, friendId):
+		user_list = User.objects.filter(id = friendId)
+		user2_list = User.objects.filter(id = myId)
+		if user_list:
+			friend = user_list[0]
+			me = user2_list[0]
+			me.friends.remove(friend)
 		return True
+
 class User(models.Model):
-
 	name = models.CharField(max_length=45)
 	last = models.CharField(max_length=45)
 	email = models.CharField(max_length=100)
 	password = models.CharField(max_length=100)
 	created_at = models.DateTimeField(auto_now_add = True)
 	updated_at = models.DateTimeField(auto_now = True)
-	friends = models.ManyToManyField("self",blank=True)
+	friends = models.ManyToManyField("self", blank=True)
 	objects = UserManager()
--- apps/belt_django_app/views.py
+++ apps/belt_django_app/views.py
@@ -1,111 +1,84 @@
-from django.shortcuts import render, HttpResponse, redirect
-from .models import User
+from django.shortcuts import render, redirect
 from django.contrib import messages
-import re
-import bcrypt
+from .models import User
 
-
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
+#=====================================
+# 				Render
+#=====================================
 def index(request):
 	return render(request,"belt_django_app/main.html")
 
-
-def register(request):
-	result = User.objects.register(request)
-	if result == True:
-		return redirect ('/')
-	if result == False:
-		return redirect ('/friends')
-
 def friends(request):
+	if 'user_id' in request.session:
 
-	if request.session['name'] == "":
-		return redirect('/')
-	else:
 		user_id = request.session['user_id']
-		users = User.objects.all().exclude(id=user_id)
-		user_list = User.objects.filter(id=user_id)
+		users = User.objects.exclude(id = user_id)
+		user_list = User.objects.filter(id = user_id)
 
-		userstoshow = user_list[0]
-		# this is showing all the friends i have
-		otherusers_list = userstoshow.friends.all()
-		# now i want to make 2 lists and have them compare to each other to see if my friends are in users then dont add them
-		alluserslist= User.objects.all()
-		# add all users in the users list
-		print 'this is alluserslist'
-		print alluserslist
-		# now do a check to see if allusers has friends
-		random = user_list[0]
-		allmyfriends = random.friends.all()
-		print 'this is allmyfriends'
-		print allmyfriends
+		usertoshow = user_list[0]
 
-		notmyfriends =[]
+		friend_ids = usertoshow.friends.values_list('id', flat=True)
+		notmyfriends = User.objects.exclude(id__in=friend_ids).exclude(id=user_id)
 
-		for user in alluserslist:
-			if user not in allmyfriends:
-				notmyfriends.append(user)
-		print 'this is notmyfriends'
-		print notmyfriends
-
-		if user_list:
-			context = {
-			'users': users,
-			'user': user_list[0],
-			'alluserslist':alluserslist,
+		context = {
+			'user': usertoshow,
 			'notmyfriends' : notmyfriends
-			}
-			return render(request,"belt_django_app/friends.html", context)
+		}
+		return render(request,"belt_django_app/friends.html", context)
 
-		else:
-			users = User.objects.all()
-			context = {
-			'users':users,
-			}
-			return render(request,"belt_django_app/friends.html", context)
-
+	return redirect('/')
 
 def showuser(request, id):
-	id = id
-	results = User.objects.filter(id=id)
-	context= {
-	"results" : results[0]
-	}
-	return render(request,"belt_django_app/showuser.html", context)
+	results = User.objects.filter(id = id)
+	if results:
+		context = {
+			"results" : results[0]
+		}
+		return render(request,"belt_django_app/showuser.html", context)
+	return redirect('/friends')
+
+#=====================================
+# 			Process
+#=====================================
 
 def addusertofriend(request, id):
 	user_id = request.session['user_id']
-	friend_id = id
-	dataObject = {
-	"user_id" : user_id,
-	"friend_id" : friend_id
-	}
-	results = User.objects.addusertofriend(dataObject)
-	if results:
-		return redirect ('/friends')
+	User.objects.addusertofriend(user_id, id)
+	return redirect ('/friends')
 
 def deletefriend(request, id):
-	friend_user_id= id
-	user_id= request.session['user_id']
-	dataObject={
-	'friend_user_id': friend_user_id,
-	'user_id': user_id
-	}
-	results = User.objects.deletefriend(dataObject)
-	if results:
-		return redirect ('/friends')
-	else:
-		return redirect ('/friends')
+	user_id = request.session['user_id']
+	User.objects.deletefriend(user_id, id)
+	return redirect ('/friends')
 
-def logout(request):
-	request.session['name'] = ""
-	request.session['user_id'] = 0
-	return redirect('/')
+#=====================================
+# 			login reg
+#=====================================
 
 def login(request):
-	result = User.objects.login(request)
-	if result == True:
-		return redirect ('/')
-	if result == False:
-		return redirect ('/friends')
+	if request.method == 'POST':
+		result = User.objects.login(request.POST)
+		if result['status']:
+			request.session['user_id'] = result['user_id']
+			request.session['name'] = result['user_name']
+			return redirect ('/friends')
+		else:
+			for errorStr in result['errors']:
+				messages.error(request, errorStr)
+	return redirect ('/')
+
+def register(request):
+	if request.method == 'POST':
+		result = User.objects.register(request.POST)
+		if result['status']:
+			request.session['user_id'] = result['user_id']
+			request.session['name'] = result['user_name']
+			return redirect ('/friends')
+		else:
+			for errorStr in result['errors']:
+				messages.error(request, errorStr)
+	return redirect ('/')
+
+def logout(request):
+	request.session.clear()
+	return redirect('/')
